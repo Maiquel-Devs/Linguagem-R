c57ef2068d3f89ed9640e239f311dc2c98cc09bd
Maiquel-Devs <junior.mafra13062005@gmail.com>
2025-03-26 17:01:54 -0300

    +

--- Conhecendo_Funções/Mais_Funções_R.R
+++ Conhecendo_Funções/Mais_Funções_R.R
@@ -29,3 +29,50 @@ pesos<-c(62,NA,76,93,49)
 mean(pesos)
 # Habilitar o NA 
 mean(pesos,na.rm = T)
+
+# Sequencias 
+
+seq(1,10)
+seq(1,10,2)
+
+seq(10,1,-1)
+
+# ou
+
+1:10
+10:1
+
+# Repetições 
+
+rep(10, 7)
+
+rep(c(1,2), 4)
+
+
+# Matrizes
+
+x <- matrix(1:10, nrow = 2, ncol = 5, byrow = T)
+
+y <- 1:10
+
+matriz <- matrix(y, nrow = 2, ncol = 5, byrow = T)
+
+# cbind " adicionar coluna "  rbind " adicionar linha "
+
+x <- matrix(1:10, nrow = 5, ncol = 2)
+
+y <- cbind(x,1:5)  #Adicionando Coluna
+print(y)
+
+z <- rbind(x, 1:2) # Adicionando Linha
+print(z)
+
+# Acessando/Extraindo linha e coluna de uma matriz
+
+print(x)
+
+z[2,]   # Extraindo Linha 
+z[,1]   # Extraindo Coluna
+
+# Dimensão " Mostra a quantidade de linha e coluna de uma Matriz "
+dim(x)
