2c6525c8eb1707409477ad8f86f9bdc6bab86cba
Maiquel-Devs <junior.mafra13062005@gmail.com>
2025-04-02 17:25:25 -0300

    +

---
+++ Conhecendo_Funções/Mais_Funções_R_Parte002.R
@@ -0,0 +1,34 @@
+# Condições IF e ELSE 
+
+# Apenas uma Condição (só o IF)
+
+if (5 == 5) print('Está Correto!')
+
+# Duas Condições na mesma linha (IF e ELSE)
+
+if (5 == 6) print('Está Correto!') else print('Não Está Correto!')
+
+# IF e ELSE de forma organizada com uso de 'chaves'
+
+if (5 == 6) {
+  print('Está Correto!')
+} else {
+  print('Não Está Correto!')
+}
+
+# Repetições do comando FOR
+
+for (i in 1:3) {
+  print('Olá Mundo!')
+}
+
+# Repetições do comando WHILE
+
+# Enquando for verdade execute a condição desejada 
+
+contador <- 0
+
+while (contador <= 10) {
+  print(contador)
+  contador <- contador + 1
+}
